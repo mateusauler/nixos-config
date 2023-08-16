5e787bf823f648bbd1200f724a9884f1c15a65a0
Mateus Auler <mateusauler@protonmail.com>
2023-08-16 04:20:31 -0300

    Add '..' alias to bash

--- home-manager-modules/bash.nix
+++ home-manager-modules/bash.nix
@@ -4,7 +4,7 @@ let cfg = config.modules.bash;
 in {
   programs.bash = {
     enable = true;
-    shellAliases = (import ./shell-aliases.nix { inherit config; }) // (import ./fish/abbreviations.nix);
+    shellAliases = (import ./shell-aliases.nix { inherit config; }) // (import ./fish/abbreviations.nix) // { ".." = "cd .."; };
     historyControl = [ "ignorespace" "ignoredups" "erasedups" ];
   };
 }
