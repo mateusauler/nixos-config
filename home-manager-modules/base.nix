8aa983907b3ef9f4c0354b20089997d7635c1250
Mateus Auler <mateusauler@protonmail.com>
2023-08-16 04:16:41 -0300

    Add environment variables to home manager

--- home-manager-modules/base.nix
+++ home-manager-modules/base.nix
@@ -32,6 +32,51 @@ in {
     inherit username;
     homeDirectory = "/home/${username}";
 
+    sessionVariables = rec {
+      TERMINAL                    = "$TERM";
+      COLORTERM                   = "$TERM";
+
+      VISUAL                      = "$EDITOR";
+      DIFFPROG                    = "meld";
+
+      MANPAGER                    = "bat -l man -p";
+
+      BROWSER                     = "browser";
+      BROWSER_PRIV                = "browser -p";
+      BROWSER_PROF                = "browser --ProfileManager";
+
+      # Fix android studio
+      CHROME_EXECUTABLE           = "`${lib.getBin pkgs.bash}/bin/bash -c 'which chromium' 2> /dev/null`";
+      _JAVA_AWT_WM_NONREPARENTING = "1";
+
+      # Cleanup ~/
+      XAUTHORITY                  = "$XDG_RUNTIME_DIR/Xauthority";
+      XINITRC                     = "${config.xdg.configHome}/X11/xinitrc";
+      LESSHISTFILE                = "-";
+      WGETRC                      = "${config.xdg.configHome}/wget/wgetrc";
+      GNUPGHOME                   = "${config.xdg.dataHome}/gnupg";
+      WINEPREFIX                  = "${config.xdg.dataHome}/wineprefixes/default";
+      GOPATH                      = "${config.xdg.dataHome}/go";
+      SQLITE_HISTORY              = "${config.xdg.dataHome}/sqlite_history";
+      CCACHE_CONFIGPATH           = "${config.xdg.configHome}/ccache.config";
+      CCACHE_DIR                  = "${config.xdg.cacheHome}/ccache";
+      ANDROID_PREFS_ROOT          = "${config.xdg.configHome}/android";
+      ADB_KEYS_PATH               = "${ANDROID_PREFS_ROOT}";
+      ANDROID_HOME                = "${config.xdg.dataHome}/android";
+      ANDROID_EMULATOR_HOME       = "${ANDROID_HOME}/emulator";
+      _JAVA_OPTIONS               = "-Djava.util.prefs.userRoot=${config.xdg.configHome}/java";
+      NPM_CONFIG_USERCONFIG       = "${config.xdg.configHome}/npm/npmrc";
+      NVM_DIR                     = "${config.xdg.dataHome}/nvm";
+      CARGO_HOME                  = "${config.xdg.dataHome}/cargo";
+      CUDA_CACHE_PATH             = "${config.xdg.cacheHome}/nv";
+      GRADLE_USER_HOME            = "${config.xdg.dataHome}/gradle";
+      JUPYTER_CONFIG_DIR          = "${config.xdg.configHome}/jupyter";
+      CABAL_CONFIG                = "${config.xdg.configHome}/cabal/config";
+      CABAL_DIR                   = "${config.xdg.cacheHome}/cabal";
+      PYTHONSTARTUP               = "${config.xdg.configHome}/python/pythonrc";
+      HISTFILE                    = "${config.xdg.stateHome}/bash/history";
+    };
+
     packages = with pkgs; [
       btop
       du-dust
