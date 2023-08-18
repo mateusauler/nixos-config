36309a88aac83a6d731d68f899df946ace3d478f
Mateus Auler <mateusauler@protonmail.com>
2023-08-18 01:57:05 -0300

    Wheatley: Install and autostart nm-applet

--- hosts/Wheatley/home.nix
+++ hosts/Wheatley/home.nix
@@ -6,7 +6,13 @@
   modules = {
     desktop.enable = true;
     waybar.battery.enable = true;
+    hyprland.extraAutostart = {
+      networkmanagerapplet = "nm-applet";
+    };
   };
+
+  # TODO: Check if the nm-applet home-manager service would be a better idea
+  home.packages = [ pkgs.networkmanagerapplet ];
 
   home.stateVersion = "22.11";
 }
