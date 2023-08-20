31a8b0e272dee9688692d47704f0e81e551951f6
Mateus Auler <mateusauler@protonmail.com>
2023-08-20 16:59:42 -0300

    Wheatley: Swap is now 8GB

--- hosts/Wheatley/hardware-configuration.nix
+++ hosts/Wheatley/hardware-configuration.nix
@@ -22,10 +22,9 @@
 
   swapDevices = [ {
     device = "/var/lib/swapfile";
-    size = 16*1024;
+    size = 8*1024;
     options = [ "nofail" ];
   } ];
-
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
