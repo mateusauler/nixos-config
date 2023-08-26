1c9c4a1c11e882d4218520caa4ced37cfbf657ff
Mateus Auler <mateusauler@protonmail.com>
2023-08-26 17:53:48 -0300

    waybar: Fix temperature reading

--- home-manager-modules/waybar/settings.nix
+++ home-manager-modules/waybar/settings.nix
@@ -149,14 +149,14 @@
   };
   "temperature" = {
     # "thermal-zone" = 2;
-    "hwmon-path" = "/sys/class/hwmon/hwmon2/temp1_input";
+    "hwmon-path" = "/sys/class/hwmon/hwmon3/temp1_input";
     "critical-threshold" = 80;
     # "format-critical" = "{temperatureC}°C {icon}";
     "format" = "{temperatureC}°C {icon}";
     "format-icons" = [
       "" "" ""
     ];
-    "interval" = 3;
+    "interval" = 1;
   };
   "backlight" = {
     # "device" = "acpi_video1";
