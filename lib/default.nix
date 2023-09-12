07d7fb5ed0fa3a20fb85789ba946fd5eeb3ba103
Mateus Auler <mateusauler@protonmail.com>
2023-09-12 17:06:20 -0300

    lib: Remove specialArgs indirection in mkNixosSystem

--- lib/default.nix
+++ lib/default.nix
@@ -18,27 +18,26 @@
             // { inherit hostname; };
       username = custom.username;
       specialArgs' = specialArgs // { inherit inputs custom; };
-    in
-      let
-        specialArgs = specialArgs';
-      in nixpkgs.lib.nixosSystem {
-        # TODO: Look into replacing system with localSystem
-        #       Suggested here: https://discordapp.com/channels/568306982717751326/741347063077535874/1140546315990859816
-        inherit system pkgs specialArgs;
+    in nixpkgs.lib.nixosSystem rec {
+      # TODO: Look into replacing system with localSystem
+      #       Suggested here: https://discordapp.com/channels/568306982717751326/741347063077535874/1140546315990859816
+      inherit system pkgs;
 
-        modules = [
-          (dir + /configuration.nix)
-          home-manager.nixosModules.home-manager
-          {
-            home-manager = {
-              users.${username} = import (dir + /home.nix);
-              useGlobalPkgs = true;
-              useUserPackages = false;
-              extraSpecialArgs = specialArgs;
-            };
-          }
-        ];
-      };
+      specialArgs = specialArgs';
+
+      modules = [
+        (dir + /configuration.nix)
+        home-manager.nixosModules.home-manager
+        {
+          home-manager = {
+            users.${username} = import (dir + /home.nix);
+            useGlobalPkgs = true;
+            useUserPackages = false;
+            extraSpecialArgs = specialArgs;
+          };
+        }
+      ];
+    };
 
   cloneRepo = { path, url, ssh-uri ? null }:
     let
