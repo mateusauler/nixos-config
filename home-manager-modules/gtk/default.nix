16b0c548387c89e231f4a8c0333608b9c2dffdf2
Mateus Auler <mateusauler@protonmail.com>
2023-09-12 21:58:24 -0300

    gtk: Refactor & organize

--- home-manager-modules/gtk/default.nix
+++ home-manager-modules/gtk/default.nix
@@ -4,16 +4,30 @@ let
   cfg = config.modules.gtk;
   nix-colors-lib = nix-colors.lib.contrib { inherit pkgs; };
 in {
-  imports = [
-    ./gtk2
-    ./gtk3
-    ./gtk4
-  ];
-
   options.modules.gtk.enable = lib.mkEnableOption "gtk";
 
   config = lib.mkIf cfg.enable {
-    gtk = {
+    gtk = let
+      commonConfigs = {
+        gtk-application-prefer-dark-theme = true;
+        gtk-decoration-layout = "menu:none";
+        gtk-enable-animations = true;
+        # TODO: Configure fonts automatically
+        gtk-font-name = "Noto Sans, 10";
+        gtk-menu-images = true;
+        gtk-primary-button-warps-slider = false;
+        gtk-titlebar-double-click = "menu";
+        gtk-titlebar-middle-click = "none";
+        gtk-titlebar-right-click = "none";
+        gtk-toolbar-icon-size = "GTK_ICON_SIZE_SMALL_TOOLBAR";
+        gtk-toolbar-style = "GTK_TOOLBAR_BOTH";
+        gtk-xft-antialias = 1;
+        gtk-xft-dpi = 98304;
+        gtk-xft-hinting = 1;
+        gtk-xft-hintstyle = "hintfull";
+        gtk-xft-rgba = "rgb";
+      };
+    in {
       enable = true;
       theme = {
         package = nix-colors-lib.gtkThemeFromScheme {
@@ -29,6 +43,25 @@ in {
         package = pkgs.papirus-icon-theme;
         name = "Papirus-Dark";
       };
+      gtk2 = {
+        configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc-2.0";
+        extraConfig = lib.strings.concatLines
+          (lib.attrsets.foldlAttrs
+            (acc: name: value:
+              acc ++ [
+                ("${name}=" + (
+                  if      builtins.typeOf value == "string" then (if lib.strings.hasPrefix "GTK_" value then value else "\"${value}\"")
+                  else if builtins.typeOf value == "bool"   then (if value then "1" else "0")
+                  else                                           toString value
+                ))
+              ]
+            )
+            [ ]
+            commonConfigs
+          );
+      };
+      gtk3.extraConfig = commonConfigs;
+      gtk4.extraConfig = commonConfigs;
     };
   };
 }
