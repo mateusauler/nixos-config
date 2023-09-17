59336cf72c4ea451b693aa071f72342af5578727
Mateus Auler <mateusauler@protonmail.com>
2023-09-17 20:29:27 -0300

    fish: Add & remove some abbreviations

--- home-manager-modules/fish/abbreviations.nix
+++ home-manager-modules/fish/abbreviations.nix
@@ -2,42 +2,47 @@
 
 let
   inherit (custom) dots-path;
-in {
-  mn  =  "udisksctl mount -b";
-  um  =  "udisksctl unmount -b";
+in
+rec {
+  mn = "udisksctl mount -b";
+  um = "udisksctl unmount -b";
 
-  g   =  "git";
+  g = "git";
 
-  gs  =  "git status";
-  gu  =  "git push";
-  gl  =  "git pull";
-  ga  =  "git add";
-  gap =  "git add -p";
-  gc  =  "git commit";
-  gca =  "git commit --amend";
-  gh  =  "git checkout";
-  gb  =  "git branch";
-  gd  =  "git diff";
-  gdc =  "git diff --cached";
-  gg  =  "git grep";
-  ggi =  "git grep -i";
+  gs = "git status";
+  gu = "git push";
+  gl = "git pull";
+  ga = "git add";
+  gap = "git add -p";
+  gc = "git commit";
+  gca = "git commit --amend";
+  gh = "git checkout";
+  gb = "git branch";
+  gd = "git diff";
+  gdc = "git diff --cached";
+  gg = "git grep";
+  ggi = "git grep -i";
+  cg = "cd (git rev-parse --show-toplevel)";
 
-  nrb  = "nixos-rebuild --verbose --flake ${dots-path} --show-trace build";
-  nrs  = "sudo nixos-rebuild --verbose --flake ${dots-path} switch";
-  nrt  = "sudo nixos-rebuild --verbose --flake ${dots-path} test";
+  nrb = "nixos-rebuild --verbose --flake ${dots-path} --show-trace build";
+  nrs = "sudo nixos-rebuild --verbose --flake ${dots-path} switch";
+  nrt = "sudo nixos-rebuild --verbose --flake ${dots-path} test";
   nrbt = "sudo nixos-rebuild --verbose --flake ${dots-path} boot";
+
+  nrbf = "${nrb} --fast";
+  nrsf = "${nrs} --fast";
+  nrtf = "${nrt} --fast";
+  nrbtf = "${nrbt} --fast";
 
   dots = "cd ${dots-path}";
 
-  pa  =  "patch -p1 <";
+  z = "zathura";
 
-  # p  =  "paru";
-  z   =  "zathura";
+  n = "nvim";
+  ni = "neovide";
 
-  n   =  "nvim";
-  ni  =  "neovide";
-
-  img =  "nsxiv -a";
+  img = "nsxiv -a";
+  im = img;
 
   mkdir = "mkdir -p";
   hexdump = "hexdump -C";
