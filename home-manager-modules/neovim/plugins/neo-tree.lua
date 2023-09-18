bdd2f220fc12d72c7679c044062ce7e3609dbb3a
Mateus Auler <mateusauler@protonmail.com>
2023-09-17 22:28:15 -0300

    neovim: Set symbols for diagnostics in neo-tree

--- home-manager-modules/neovim/plugins/neo-tree.lua
+++ home-manager-modules/neovim/plugins/neo-tree.lua
@@ -1,3 +1,8 @@
+vim.fn.sign_define("DiagnosticSignError", {text = " ", texthl = "DiagnosticSignError"})
+vim.fn.sign_define("DiagnosticSignWarn",  {text = " ", texthl = "DiagnosticSignWarn"})
+vim.fn.sign_define("DiagnosticSignInfo",  {text = " ", texthl = "DiagnosticSignInfo"})
+vim.fn.sign_define("DiagnosticSignHint",  {text = "󰌵 ", texthl = "DiagnosticSignHint"})
+
 local config = {
   log_level = "warn",
   popup_border_style = "rounded",
@@ -21,12 +26,6 @@ local config = {
   },
   default_component_configs = {
     diagnostics = {
-      symbols = {
-        hint = "H",
-        info = "I",
-        warn = "!",
-        error = "X",
-      },
       highlights = {
         hint = "DiagnosticSignHint",
         info = "DiagnosticSignInfo",
