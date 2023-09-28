5860cf7911bb8f5321a47369540bdd06b3adf04e
Mateus Auler <mateusauler@protonmail.com>
2023-09-28 20:15:52 -0300

    Wheatley: Fix disabling of power-menu firmware action

--- hosts/Wheatley/home.nix
+++ hosts/Wheatley/home.nix
@@ -10,7 +10,7 @@
       networkmanagerapplet = "nm-applet";
     };
     git.gpgKey = "5D9269C32D1E49D278A4410C263716004E5EF18D";
-    power-menu.actions.firmware.enable = false;
+    power-menu.actions.set.firmware.enable = false;
   };
 
   # TODO: Check if the nm-applet home-manager service would be a better idea
