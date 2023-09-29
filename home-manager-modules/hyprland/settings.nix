587ca5b6e4d7cd8004d651f4af48c77a49625f0d
Mateus Auler <mateusauler@protonmail.com>
2023-09-29 01:55:40 -0300

    hyprland: Rules for steam games

--- home-manager-modules/hyprland/settings.nix
+++ home-manager-modules/hyprland/settings.nix
@@ -110,6 +110,11 @@ in
 
     "monitor $mon2,   class:(org\\.qbittorrent\\.qBittorrent)"
     "workspace empty silent, class:(org\\.qbittorrent\\.qBittorrent) title:^(qBittorrent v([0-9]\\.){2}[0-9])$"
+
+    # Steam games
+    "monitor $mon1, class:(steam_app_.*)"
+    "workspace 9,   class:(steam_app_.*)"
+    "fullscreen,    class:(steam_app_.*)"
   ];
 
   layerrule = [
