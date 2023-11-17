f3e6c18452ffe53362ca43eaa80ea0d4399c1719
Mateus Auler <mateusauler@protonmail.com>
2023-11-16 23:48:26 -0300

    meld: Set git mergetool program to meld

--- home-manager-modules/meld.nix
+++ home-manager-modules/meld.nix
@@ -6,6 +6,7 @@ in {
   options.modules.meld.enable = lib.mkEnableOption "meld";
 
   config = lib.mkIf cfg.enable {
+    programs.git.extraConfig.merge.tool = "meld";
     home = {
       sessionVariables = { DIFFPROG = "meld"; };
       packages = [ pkgs.meld ];
