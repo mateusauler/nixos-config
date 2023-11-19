a31cc05cfa4ebd98bed63968d84004cb97d18e64
Mateus Auler <mateusauler@protonmail.com>
2023-11-19 16:00:17 -0300

    fish: Change shell theme before running pfetch
    Before, pfetch's output was getting cleaned when running on the tty

--- home-manager-modules/fish/default.nix
+++ home-manager-modules/fish/default.nix
@@ -5,7 +5,8 @@ let
   nix-colors-lib = nix-colors.lib.contrib { inherit pkgs; };
   inherit (lib) mkDefault;
   inherit (pkgs.lib) mkTrueEnableOption;
-in {
+in
+{
   options.modules.fish = {
     enable = lib.mkEnableOption "fish";
     pfetch.enable = mkTrueEnableOption "pfetch";
@@ -23,22 +24,23 @@ in {
         shellAbbrs = (import ./abbreviations.nix args);
 
         interactiveShellInit = ''
-          ${lib.strings.optionalString cfg.pfetch.enable "pfetch"}
           sh ${nix-colors-lib.shellThemeFromScheme { scheme = config.colorScheme; }}
+          ${lib.strings.optionalString cfg.pfetch.enable "pfetch"}
         '';
 
         shellInit = ''
           set fish_greeting
-          fish_vi_key_bindings'';
+          fish_vi_key_bindings
+        '';
       };
     };
 
     # The `fish_variables` file includes all of the abbreviations and some other variables that I don't care about.
     # So, if an abbreviation is removed in the config, it will still exist in the fish_variables file.
     # Therefore, let's remove it.
-    home.activation."remove-fish_variables" = lib.hm.dag.entryAfter ["writeBoundary"] ''
-        $DRY_RUN_CMD rm -f $VERBOSE_ARG ${config.xdg.configHome}/fish/fish_variables
-      '';
+    home.activation."remove-fish_variables" = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      $DRY_RUN_CMD rm -f $VERBOSE_ARG ${config.xdg.configHome}/fish/fish_variables
+    '';
 
     xdg.configFile = {
       "fish/functions" = {
