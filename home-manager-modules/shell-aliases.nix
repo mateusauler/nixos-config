0f42ba9cc0495c048c62b12e56fa05c86b8ef160
Mateus Auler <mateusauler@protonmail.com>
2023-11-20 14:50:35 -0300

    shell: Add lt (eza --tree) aliases

--- home-manager-modules/shell-aliases.nix
+++ home-manager-modules/shell-aliases.nix
@@ -1,19 +1,25 @@
 { config, lib, ... }:
 
 {
-  gua =	"git remote | sed '/^blacklist/{d}' | xargs -L1 git push --all";
-  gsu	= "git remote | sed '/^blacklist/{d}' | xargs -I {} git branch -u {}/(git branch --show-current) (git branch --show-current)";
+  gua = "git remote | sed '/^blacklist/{d}' | xargs -L1 git push --all";
+  gsu = "git remote | sed '/^blacklist/{d}' | xargs -I {} git branch -u {}/(git branch --show-current) (git branch --show-current)";
   cleanup-after-bad-patch = "rm *.{rej, orig}";
   wget = "wget --hsts-file $XDG_CACHE_HOME/wget-hsts";
-  sqlite3	= "sqlite3 -init $XDG_CONFIG_HOME/sqlite3/sqliterc";
+  sqlite3 = "sqlite3 -init $XDG_CONFIG_HOME/sqlite3/sqliterc";
 
-  l   = "ls";
-  la  = "ls -a";
-  ll  = "ls -lFb";
+  l = "ls";
+  la = "ls -a";
+  ll = "ls -lFb";
   lla = "ll -a";
 
   rm = "rm -I";
   cp = "cp -r";
 } // lib.attrsets.optionalAttrs
   config.programs.eza.enable
-  { ls = "eza --group-directories-first --icons"; }
+  rec {
+    ls = "eza --group-directories-first --icons";
+    lt = "${ls} --tree";
+    llt = "${lt} -l";
+    lat = "${lt} -a";
+    llat = "${lt} -la";
+  }
