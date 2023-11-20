d078750258448ce015147ca345e52be569d90f56
Mateus Auler <mateusauler@protonmail.com>
2023-11-19 23:46:39 -0300

    shell: Add name

--- shell.nix
+++ shell.nix
@@ -10,6 +10,7 @@
 , ...
 }: {
   default = pkgs.mkShell {
+    name = "nix bootstrap";
     NIX_CONFIG = "extra-experimental-features = nix-command flakes repl-flake";
     shellHook = "exec $SHELL";
     nativeBuildInputs = with pkgs; [
