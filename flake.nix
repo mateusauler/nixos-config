49e87a17019e870f52c1f1e2b3ea5f84932354e1
Mateus Auler <mateusauler@protonmail.com>
2023-11-24 13:32:24 -0300

    flake: Use stable nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My NixOS config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
 
     sops-nix = {
       url = "github:mic92/sops-nix";
@@ -10,7 +10,7 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
