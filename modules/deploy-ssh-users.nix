14cf679bc534aa844634000d73d2693475fa93ff
Mateus Auler <mateusauler@protonmail.com>
2023-11-29 11:20:30 -0300

    deploy-ssh-users: Deploy without a systemd service

--- modules/deploy-ssh-users.nix
+++ modules/deploy-ssh-users.nix
@@ -1,56 +1,27 @@
 { config, lib, pkgs, ... }:
 
 let
-  foldlUsers = pkgs.lib.foldlUsers config;
-  forEachKeyType = { acc ? { }, fn }: lib.foldl' fn acc [ "ed25519" "rsa" ];
+  foldlKeyTypes = { acc ? { }, fn }: lib.foldl' fn acc [ "ed25519" "rsa" ];
+  foldlUsersKeys = f: pkgs.lib.foldlUsers config { fn = acc: name: user: acc // foldlKeyTypes { fn = acc: f acc name user; }; };
   cfg = config.modules.deploy-ssh-users;
 in
 {
   options.modules.deploy-ssh-users.enable = lib.mkEnableOption "Deploy user ssh keys using sops";
 
   config = lib.mkIf cfg.enable {
-    sops.secrets = foldlUsers {
-      fn = acc: name: _: acc // forEachKeyType {
-        fn = acc: k: acc // {
-          "ssh/${name}/keys/${k}" = {
-            sopsFile = ../users/secrets.yaml;
-            owner = name;
-          };
-        };
+    sops.secrets = foldlUsersKeys (acc: name: user: k: acc // {
+      "ssh/${name}/keys/${k}" = {
+        sopsFile = ../users/secrets.yaml;
+        owner = name;
+        path = "${user.home}/.ssh/id_${k}";
       };
-    };
+    });
 
-    systemd.user.services = foldlUsers {
-      fn = acc: name: user: acc // {
-        "deploy-ssh-keys-${name}" = {
-          script = forEachKeyType {
-            acc = "";
-            fn = acc: k:
-              let
-                secret-path = config.sops.secrets."ssh/${name}/keys/${k}".path;
-                ssh-dir = "${user.home}/.ssh";
-                dest-path = "${ssh-dir}/id_${k}";
-              in
-              acc +
-                ''
-                  if [ -s "${secret-path}" ] ; then
-                    echo Deploying ${k}...
-
-                    # Import the key
-                    mkdir -p ${ssh-dir}
-                    cp -f ${../users/${name}/id_${k}.pub} ${dest-path}.pub
-                    cp -f "${secret-path}" ${dest-path}
-                    chmod u+w ${dest-path} ${dest-path}.pub
-
-                    echo Finished
-                  else
-                    echo Didn\'t deploy ${k}
-                  fi
-                '';
-          };
-          wantedBy = [ "multi-user.target" ];
-        };
+    home-manager.users = foldlUsersKeys (acc: name: _: k: lib.recursiveUpdate acc {
+      ${name}.home.file."ssh_id_${k}.pub" = {
+        target = ".ssh/id_${k}.pub";
+        source = ../users + "/${name}/id_${k}.pub";
       };
-    };
+    });
   };
 }
