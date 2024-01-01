0fc8836da2f43cf03fc486b67e103b2b99b22fa5
Mateus Auler <mateusauler@protonmail.com>
2024-01-01 12:00:24 -0300

    change-wallpaper: Use full program paths for commands

--- home-manager-modules/scripts/change-wallpaper.nix
+++ home-manager-modules/scripts/change-wallpaper.nix
@@ -7,51 +7,56 @@ let
   dest = "${pics-dir}/wallpaper";
   set-wallpaper-command = lib.strings.optionalString (cfg.command != null) "${cfg.command} ${dest}";
   nix-colors-lib = nix-colors.lib.contrib { inherit pkgs; };
-in {
+in
+{
   options.modules.change-wallpaper = {
-	  enable = lib.mkEnableOption "change-wallpaper";
-	  command = with lib.types; lib.mkOption {
-	    type = nullOr str;
-	    default = null;
-	  };
+    enable = lib.mkEnableOption "change-wallpaper";
+    command = with lib.types; lib.mkOption {
+      type = nullOr str;
+      default = null;
+    };
   };
 
   config = lib.mkIf cfg.enable {
     home = {
       activation = {
-        clone-wallpapers = lib.hm.dag.entryAfter ["writeBoundary"] (
+        clone-wallpapers = lib.hm.dag.entryAfter [ "writeBoundary" ] (
           pkgs.lib.cloneRepo {
             path = wall-dir;
             url = "https://github.com/mateusauler/wallpapers";
           }
         );
         set-default-wallpaper =
-        let
-          default-wallpaper = nix-colors-lib.nixWallpaperFromScheme {
-            scheme = config.colorScheme;
-            width = 3840;
-            height = 2160;
-            logoScale = 5.0;
-          };
-          link-wallpaper = ''
-            if [ ! -e ${dest} ]; then
-              $DRY_RUN_CMD ln $VERBOSE_ARG -s ${default-wallpaper} ${dest}
-              [ ! -z "$DISPLAY" ] && $DRY_RUN_CMD ${set-wallpaper-command}
-            fi
-          '';
-        in
-          lib.hm.dag.entryAfter ["writeBoundary" "clone-wallpapers"] link-wallpaper;
+          let
+            default-wallpaper = nix-colors-lib.nixWallpaperFromScheme {
+              scheme = config.colorScheme;
+              width = 3840;
+              height = 2160;
+              logoScale = 5.0;
+            };
+            link-wallpaper = ''
+              if [ ! -e ${dest} ]; then
+                $DRY_RUN_CMD ln $VERBOSE_ARG -s ${default-wallpaper} ${dest}
+                [ ! -z "$DISPLAY" ] && $DRY_RUN_CMD ${set-wallpaper-command}
+              fi
+            '';
+          in
+          lib.hm.dag.entryAfter [ "writeBoundary" "clone-wallpapers" ] link-wallpaper;
       };
 
       packages =
-      let
-        change-wallpaper = pkgs.writeShellScriptBin "chw" ''
-          file=$(find ${wall-dir} -type f | shuf | ${pkgs.nsxiv}/bin/nsxiv -oiqt | head -n 1)
-          [ ! -z $file ] && ln -sf $file ${dest}
-          ${set-wallpaper-command}
-        '';
-      in
-      [ change-wallpaper ];
+        let
+          find = "${pkgs.findutils}/bin/find";
+          head = "${pkgs.coreutils}/bin/head";
+          nsxiv = "${pkgs.nsxiv}/bin/nsxiv";
+          shuf = "${pkgs.coreutils}/bin/shuf";
+          change-wallpaper = pkgs.writeShellScriptBin "chw" ''
+            file=$(${find} ${wall-dir} -type f | ${shuf} | ${nsxiv} -oiqt | ${head} -n 1)
+            [ ! -z $file ] && ln -sf $file ${dest}
+            ${set-wallpaper-command}
+          '';
+        in
+        [ change-wallpaper ];
     };
 
     xdg.desktopEntries.chw = {
