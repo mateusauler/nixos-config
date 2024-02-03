4d55b5521efd78e809b210e7a7fd903df36871d4
Mateus Auler <mateusauler@protonmail.com>
2024-02-03 00:21:41 -0300

    wally: Format file

--- modules/wally.nix
+++ modules/wally.nix
@@ -2,7 +2,8 @@
 
 let
   cfg = config.modules.wally;
-in {
+in
+{
   options.modules.wally = {
     enable = lib.mkEnableOption "wally";
     package = lib.mkOption { default = pkgs.wally-cli; };
@@ -11,16 +12,16 @@ in {
 
   config = lib.mkIf cfg.enable {
     environment.systemPackages =
-    let
-      wally = pkgs.writeShellScriptBin "wll" ''
-        if [ $# -ge 1 ]; then
-          ${cfg.package}/bin/${cfg.executableName} "$@" 2> /dev/null
-          sleep 1
-          rm "$@"
-        fi
-      '';
-    in
-    [ wally ];
+      let
+        wally = pkgs.writeShellScriptBin "wll" ''
+          if [ $# -ge 1 ]; then
+            ${cfg.package}/bin/${cfg.executableName} "$@" 2> /dev/null
+            sleep 1
+            rm "$@"
+          fi
+        '';
+      in
+      [ wally ];
     hardware.keyboard.zsa.enable = true;
   };
 }
