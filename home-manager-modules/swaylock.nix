e6061b85d400212ee529a40a6cddb66f5249112b
Mateus Auler <mateusauler@protonmail.com>
2024-02-07 09:55:02 -0300

    swaylock: Use nix-colors

--- home-manager-modules/swaylock.nix
+++ home-manager-modules/swaylock.nix
@@ -8,24 +8,28 @@ in
 
   config = lib.mkIf cfg.enable {
     home.packages = [ pkgs.swaylock-effects ];
-    # TODO: Use nix-colors
     # TODO: Allow for extra configurations
-    xdg.configFile."swaylock/config".text = ''
-      screenshots
+    xdg.configFile."swaylock/config".text = with config.colorScheme.colors; ''
       clock
+      screenshots
+      show-failed-attempts
+
+      fade-in=0.2
+      grace=0
+
       indicator
       indicator-radius=100
       indicator-thickness=7
-      effect-blur=30x8
+
+      effect-pixelate=100
       effect-vignette=0.5:0.5
-      ring-color=bb00cc
-      key-hl-color=880033
+
+      inside-color=${base00}88
+      key-hl-color=${base04}
       line-color=00000000
-      inside-color=00000088
+      ring-color=${base0D}
       separator-color=00000000
-      grace=0
-      fade-in=0.2
-      show-failed-attempts
+      text-color=${base05}
     '';
   };
 }
