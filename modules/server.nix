c759b97d54a282f54c3d26381b3f02dbd854c22d
Mateus Auler <mateusauler@protonmail.com>
2024-02-11 19:09:07 -0300

    server: Add commented-out sudo config for password-less rebuilds

--- modules/server.nix
+++ modules/server.nix
@@ -12,6 +12,27 @@ in
 
     security.sudo = {
       wheelNeedsPassword = false;
+      # FIXME: This should allow for password-less sudo when rebuilding the system. But it does not work.
+      # extraRules =
+      #   let
+      #     storePrefix = "/nix/store/*";
+      #     systemName = "nixos-system-${config.networking.hostName}-*";
+      #   in
+      #   [
+      #     {
+      #       commands = [
+      #         {
+      #           command = "${storePrefix}-nix-*/bin/nix-env -p /nix/var/nix/profiles/system --set ${storePrefix}-${systemName}";
+      #           options = [ "NOPASSWD" ];
+      #         }
+      #         {
+      #           command = "${storePrefix}-${systemName}/bin/switch-to-configuration";
+      #           options = [ "NOPASSWD" ];
+      #         }
+      #       ];
+      #       groups = [ "wheel" ];
+      #     }
+      #   ];
     };
 
     modules.deploy-secrets = {
