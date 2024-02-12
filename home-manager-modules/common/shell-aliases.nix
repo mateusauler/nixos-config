f76dd557d56ffe1fae3c0ab6ef7bb2105ee993f5
Mateus Auler <mateusauler@protonmail.com>
2024-02-12 02:30:48 -0300

    shell: Fix shell aliases

--- home-manager-modules/common/shell-aliases.nix
+++ home-manager-modules/common/shell-aliases.nix
@@ -4,10 +4,10 @@ let
   cfg = config.shell-aliases;
 in
 {
-  options.shell-aliases = lib.mkOption { default = { }; };
+  options.shell-aliases = lib.mkOption { type = with lib.types; attrsOf str; };
 
   config = {
-    shell-aliases = lib.mkDefault {
+    shell-aliases = {
       gua = "git remote | sed '/^blacklist/{d}' | xargs -L1 git push --all";
       gsu = "git remote | sed '/^blacklist/{d}' | xargs -I {} git branch -u {}/(git branch --show-current) (git branch --show-current)";
       cleanup-after-bad-patch = "rm *.{rej, orig}";
