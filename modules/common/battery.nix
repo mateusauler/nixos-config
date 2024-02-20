f1c226399f7871c95d331e73191639d25e961c40
Mateus Auler <mateusauler@protonmail.com>
2024-02-20 11:18:18 -0300

    battery: Add some more configs

--- modules/common/battery.nix
+++ modules/common/battery.nix
@@ -19,7 +19,13 @@ in
       settings = {
         CPU_BOOST_ON_AC = 1;
         CPU_BOOST_ON_BAT = 0;
-        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_DRIVER_OPMODE_ON_AC = "passive";
+        CPU_DRIVER_OPMODE_ON_BAT = "passive";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "default";
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+        CPU_HWP_DYN_BOOST_ON_AC = 1;
+        CPU_HWP_DYN_BOOST_ON_BAT = 0;
+        CPU_SCALING_GOVERNOR_ON_AC = "ondemand";
         CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
       };
     };
