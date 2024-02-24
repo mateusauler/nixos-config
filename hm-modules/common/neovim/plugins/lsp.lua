19beb95acd0fc4418d2bc8db768a38609c7e06e0
Mateus Auler <mateusauler@protonmail.com>
2024-02-24 19:17:31 -0300

    neovim: Add more telescope binds

--- hm-modules/common/neovim/plugins/lsp.lua
+++ hm-modules/common/neovim/plugins/lsp.lua
@@ -12,9 +12,9 @@ local on_attach = function(_, bufnr)
 	bufmap('gI',        vim.lsp.buf.implementation)
 	bufmap('<leader>D', vim.lsp.buf.type_definition)
 
-	-- bufmap('gr',        require('telescope.builtin').lsp_references)
-	-- bufmap('<leader>s', require('telescope.builtin').lsp_document_symbols)
-	-- bufmap('<leader>S', require('telescope.builtin').lsp_dynamic_workspace_symbols)
+	bufmap('gr',        require('telescope.builtin').lsp_references)
+	bufmap('<leader>s', require('telescope.builtin').lsp_document_symbols)
+	bufmap('<leader>S', require('telescope.builtin').lsp_dynamic_workspace_symbols)
 
 	bufmap('K', vim.lsp.buf.hover)
 
