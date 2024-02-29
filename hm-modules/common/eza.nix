cd095b9684df3ff78824d468ae69fbec9246e9cb
Mateus Auler <mateusauler@protonmail.com>
2024-02-28 21:18:25 -0300

    eza: Shows git status by default

--- hm-modules/common/eza.nix
+++ hm-modules/common/eza.nix
@@ -8,7 +8,7 @@ in
   config = lib.mkIf cfg.enable {
     programs.eza.enable = true;
     shell-aliases = rec {
-      ls = "eza --group-directories-first --icons";
+      ls = "eza --group-directories-first --icons --git";
       lt = "${ls} --tree";
       llt = "${lt} -l";
       lat = "${lt} -a";
