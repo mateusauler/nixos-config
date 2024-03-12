4828dd6144345c0ca1c362e77c9f8b07400a676b
Mateus Auler <mateusauler@protonmail.com>
2024-03-12 20:37:29 -0300

    waybar: Properly split into modules & refactor

--- hm-modules/desktop/waybar/settings.nix
+++ hm-modules/desktop/waybar/settings.nix
@@ -1,158 +1,163 @@
 { config, lib, ... }:
 
-{
-  layer   = "top";
-  height  = 30;
-  spacing = 8;
+let
+  cfg = config.modules.waybar;
+in
+lib.mkIf cfg.enable {
+  programs.waybar.settings.mainBar = {
+    layer   = "top";
+    height  = 30;
+    spacing = 8;
 
-  modules-left = [
-    "hyprland/workspaces"
-    "hyprland/window"
-  ];
+    modules-left = [
+      "hyprland/workspaces"
+      "hyprland/window"
+    ];
 
-  modules-center = [
-    "clock"
-  ];
+    modules-center = [
+      "clock"
+    ];
 
-  modules-right = [
-    "idle_inhibitor"
-    "pulseaudio"
-    "network"
-    "cpu"
-    "memory"
-    "temperature"
-    "keyboard-state"
-  ] ++ lib.optional config.battery.enable "battery" ++ [
-    "tray"
-  ];
+    modules-right = lib.flatten [
+      "idle_inhibitor"
+      "pulseaudio"
+      "network"
+      "cpu"
+      "memory"
+      "temperature"
+      "keyboard-state"
+      (lib.optional cfg.battery.enable "battery")
+      "tray"
+    ];
 
-  # Modules configuration
-  "hyprland/window" = {
-    max-length = 200;
-    separate-outputs = true;
-    format = "{title}";
-  };
-
-  "hyprland/workspaces" = {
-    disable-scroll = false;
-    on-click = "activate";
-    on-scroll-up   = "hyprctl dispatch workspace m+1";
-    on-scroll-down = "hyprctl dispatch workspace m-1";
-    sort-by-number = true;
-  };
-
-  keyboard-state = {
-    numlock = true;
-    capslock = true;
-    format = "{name} {icon}";
-    format-icons = {
-      locked   = "";
-      unlocked = "";
+    # Modules configuration
+    "hyprland/window" = {
+      max-length = 200;
+      separate-outputs = true;
+      format = "{title}";
     };
-  };
 
-  idle_inhibitor = {
-    format = "{icon}";
-    format-icons = {
-      activated = "";
-      deactivated = "";
+    "hyprland/workspaces" = {
+      disable-scroll = false;
+      on-click = "activate";
+      on-scroll-up   = "hyprctl dispatch workspace m+1";
+      on-scroll-down = "hyprctl dispatch workspace m-1";
+      sort-by-number = true;
     };
-  };
 
-  tray = {
-    # icon-size = 21;
-    spacing = 10;
-  };
-
-  clock = {
-    format = "  {:%H:%M:%S    %a %d/%m/%Y}";
-    interval = 1;
-
-    tooltip-format = "<big>{:%Y}</big>\n\n<tt><small>{calendar}</small></tt>";
-    calendar = {
-      mode = "month";
-      mode-mon-col = 3;
-      on-scroll = 1;
-      format = {
-        months =   "<span color='#ffead3'><b>{}</b></span>";
-        days =     "<span color='#ecc6d9'><b>{}</b></span>";
-        weeks =    "<span color='#99ffdd'><b>W{}</b></span>";
-        weekdays = "<span color='#ffcc66'><b>{}</b></span>";
-        today =    "<span color='#ff6699'><b><u>{}</u></b></span>";
+    keyboard-state = {
+      numlock = true;
+      capslock = true;
+      format = "{name} {icon}";
+      format-icons = {
+        locked   = "";
+        unlocked = "";
       };
     };
-    actions =  {
-      on-click-right    = "mode";
-      on-click-forward  = "tz_up";
-      on-click-backward = "tz_down";
-      on-scroll-up      = "shift_up";
-      on-scroll-down    = "shift_down";
+
+    idle_inhibitor = {
+      format = "{icon}";
+      format-icons = {
+        activated = "";
+        deactivated = "";
+      };
     };
-  };
 
-  cpu = {
-    format = "{usage}% ";
-    tooltip = false;
-    interval = 2;
-  };
-
-  memory = {
-    format = "{}% ";
-  };
-
-  temperature = {
-    # thermal-zone = 2;
-    hwmon-path = "/sys/class/hwmon/hwmon3/temp1_input";
-    critical-threshold = 80;
-    format = "{temperatureC}°C {icon}";
-    format-icons = [
-      "" "" "" "" ""
-    ];
-    interval = 1;
-  };
-
-  battery = {
-    states = {
-      warning = 30;
-      critical = 15;
+    tray = {
+      # icon-size = 21;
+      spacing = 10;
     };
-    format          = "{capacity}% {icon}";
-    format-charging = "{capacity}% ";
-    format-plugged  = "{capacity}% ";
-    format-alt      = "{time} {icon}";
-    format-icons    = [ "" "" "" "" "" ];
-  };
 
-  "battery#bat2" = {
-    bat = "BAT2";
-  };
+    clock = {
+      format = "  {:%H:%M:%S    %a %d/%m/%Y}";
+      interval = 1;
 
-  network = {
-    format-wifi         = "{essid} ({signalStrength}%) ";
-    format-ethernet     = "{ipaddr}/{cidr} ";
-    tooltip-format      = "{ifname} via {gwaddr} ";
-    format-linked       = "{ifname} (No IP) ";
-    format-disconnected = "Disconnected ⚠";
-    format-alt          = "{ifname}: {ipaddr}/{cidr}";
-    interval            = 5;
-  };
-
-  pulseaudio = {
-    format                 = "{icon} {volume}% {format_source}";
-    format-bluetooth       = "{volume}% {icon} {format_source}";
-    format-bluetooth-muted = " {icon} {format_source}";
-    format-muted           = " {format_source}";
-    format-source          = "| {volume}% ";
-    format-source-muted    = "| ";
-    format-icons = {
-      headphone  = " ";
-      hands-free = " ";
-      headset    = " ";
-      phone      = " ";
-      portable   = " ";
-      car        = " ";
-      default    = [ " " " " " " ];
+      tooltip-format = "<big>{:%Y}</big>\n\n<tt><small>{calendar}</small></tt>";
+      calendar = {
+        mode = "month";
+        mode-mon-col = 3;
+        on-scroll = 1;
+        format = {
+          months =   "<span color='#ffead3'><b>{}</b></span>";
+          days =     "<span color='#ecc6d9'><b>{}</b></span>";
+          weeks =    "<span color='#99ffdd'><b>W{}</b></span>";
+          weekdays = "<span color='#ffcc66'><b>{}</b></span>";
+          today =    "<span color='#ff6699'><b><u>{}</u></b></span>";
+        };
+      };
+      actions =  {
+        on-click-right    = "mode";
+        on-click-forward  = "tz_up";
+        on-click-backward = "tz_down";
+        on-scroll-up      = "shift_up";
+        on-scroll-down    = "shift_down";
+      };
     };
-    on-click = "pavucontrol";
+
+    cpu = {
+      format = "{usage}% ";
+      tooltip = false;
+      interval = 2;
+    };
+
+    memory = {
+      format = "{}% ";
+    };
+
+    temperature = {
+      # thermal-zone = 2;
+      hwmon-path = "/sys/class/hwmon/hwmon3/temp1_input";
+      critical-threshold = 80;
+      format = "{temperatureC}°C {icon}";
+      format-icons = [
+        "" "" "" "" ""
+      ];
+      interval = 1;
+    };
+
+    battery = {
+      states = {
+        warning = 30;
+        critical = 15;
+      };
+      format          = "{capacity}% {icon}";
+      format-charging = "{capacity}% ";
+      format-plugged  = "{capacity}% ";
+      format-alt      = "{time} {icon}";
+      format-icons    = [ "" "" "" "" "" ];
+    };
+
+    "battery#bat2" = {
+      bat = "BAT2";
+    };
+
+    network = {
+      format-wifi         = "{essid} ({signalStrength}%) ";
+      format-ethernet     = "{ipaddr}/{cidr} ";
+      tooltip-format      = "{ifname} via {gwaddr} ";
+      format-linked       = "{ifname} (No IP) ";
+      format-disconnected = "Disconnected ⚠";
+      format-alt          = "{ifname}: {ipaddr}/{cidr}";
+      interval            = 5;
+    };
+
+    pulseaudio = {
+      format                 = "{icon} {volume}% {format_source}";
+      format-bluetooth       = "{volume}% {icon} {format_source}";
+      format-bluetooth-muted = " {icon} {format_source}";
+      format-muted           = " {format_source}";
+      format-source          = "| {volume}% ";
+      format-source-muted    = "| ";
+      format-icons = {
+        headphone  = " ";
+        hands-free = " ";
+        headset    = " ";
+        phone      = " ";
+        portable   = " ";
+        car        = " ";
+        default    = [ " " " " " " ];
+      };
+      on-click = "pavucontrol";
+    };
   };
 }
---
+++ hm-modules/desktop/waybar/style.nix
@@ -0,0 +1,159 @@
+{ config, lib, osConfig, pkgs, ... }:
+
+let
+  inherit (pkgs.lib) colorToRgba;
+  inherit (config.programs.waybar.settings.mainBar) spacing;
+  cfg = config.modules.waybar;
+
+in
+lib.mkIf cfg.enable {
+  programs.waybar.style = with config.colorScheme.palette; with osConfig.defaultFonts; /* css */ ''
+    * {
+      font-family: FontAwesome, ${sans.name}, Roboto, Helvetica, Arial, sans-serif;
+      font-size: ${toString (sans.size - 1)}pt;
+    }
+
+    window#waybar {
+      background-color: ${colorToRgba base01 0.5};
+      color: #${base05};
+      transition: box-shadow .5s;
+    }
+
+    window#waybar.hidden {
+      opacity: 0.2;
+    }
+
+    button {
+      /* Use box-shadow instead of border so the text isn't offset */
+      box-shadow: inset 0 -3px transparent;
+      /* Avoid rounded borders under each button name */
+      border: none;
+      border-radius: 0;
+    }
+
+    #workspaces button {
+      padding: 0 5px;
+      background-color: transparent;
+      color: #${base05};
+      transition: box-shadow .5s, background-color .5s;
+    }
+
+    #workspaces button:hover {
+      background: ${colorToRgba base03 0.6};
+    }
+
+    #workspaces button.active {
+      background-color: #${base03};
+      box-shadow: inset 0 -3px #${base05};
+    }
+
+    #workspaces button.urgent {
+      background-color: #${base08};
+    }
+
+    #mode {
+      background-color: #${base03};
+      border-bottom: 3px solid #${base05};
+    }
+
+    #clock,
+    #battery,
+    #cpu,
+    #memory,
+    #disk,
+    #temperature,
+    #backlight,
+    #network,
+    #pulseaudio,
+    #wireplumber,
+    #custom-media,
+    #tray,
+    #mode,
+    #idle_inhibitor,
+    #scratchpad,
+    #mpd,
+    #keyboard-state {
+      padding: 0 10px;
+      color: #${base05};
+      border-bottom: 3px solid #${base05};
+    }
+
+    #window,
+    #workspaces {
+      margin: 0 4px;
+    }
+
+    #window {
+      box-shadow: inset 0 -3px #${base05};
+      transition: box-shadow .5s, color .5s;
+    }
+
+    window#waybar.empty #window {
+      color: transparent;
+      box-shadow: none;
+      transition: none;
+    }
+
+    /* If workspaces is the leftmost module, omit left margin */
+    .modules-left > widget:first-child > #workspaces {
+      margin-left: 0;
+    }
+
+    /* If workspaces is the rightmost module, omit right margin */
+    .modules-right > widget:last-child > #workspaces {
+      margin-right: 0;
+    }
+
+    /*
+     * Hacky way to ensure that there is a gap arround the center modules,
+     * if the screen is too small.
+     */
+    .modules-center {
+      margin-right: ${toString spacing}px;
+      margin-left:  ${toString spacing}px;
+    }
+
+    @keyframes blink {
+        to {
+          background-color: #${base05};
+          color: #${base00};
+        }
+    }
+
+    #battery.critical:not(.charging) {
+      background-color: #${base08};
+      animation-name: blink;
+      animation-duration: 0.5s;
+      animation-timing-function: linear;
+      animation-iteration-count: infinite;
+      animation-direction: alternate;
+    }
+
+    label:focus {
+      background-color: #${base00};
+    }
+
+    #tray > .passive {
+      -gtk-icon-effect: dim;
+    }
+
+    #language {
+      padding: 0 5px;
+      margin: 0 5px;
+      min-width: 16px;
+    }
+
+    #keyboard-state {
+      padding: 0 10px 0 5px;
+    }
+
+    #keyboard-state > label {
+      padding: 0 5px;
+      transition: background-color .3s;
+    }
+
+    #keyboard-state > label.locked {
+      background: #${base08};
+    }
+  '';
+}
