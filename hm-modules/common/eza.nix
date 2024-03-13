d8e5b45654f172dfcc695155b8787ca6f97cc6d4
Mateus Auler <mateusauler@protonmail.com>
2024-03-13 10:17:43 -0300

    eza: Use module settings

--- hm-modules/common/eza.nix
+++ hm-modules/common/eza.nix
@@ -6,13 +6,16 @@ in
 {
   options.modules.eza.enable = pkgs.lib.mkTrueEnableOption "eza";
   config = lib.mkIf cfg.enable {
-    programs.eza.enable = true;
-    shell-aliases = rec {
-      ls = "eza --group-directories-first --icons --git";
-      lt = "${ls} --tree";
-      llt = "${lt} -l";
-      lat = "${lt} -a";
-      llat = "${lt} -la";
+    programs.eza = {
+      enable = true;
+      git = true;
+      icons = true;
+      extraOptions = [ "--group-directories-first" ];
+    };
+    shell-aliases = {
+      llt = "lt -l";
+      lat = "lt -a";
+      llat = "lt -la";
     };
   };
 }
