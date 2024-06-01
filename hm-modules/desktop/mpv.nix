482145d0a694dc2fd99fd9abf63750c21e3f551c
Mateus Auler <mateusauler@protonmail.com>
2024-06-01 01:45:30 -0300

    mpv: Add some scripts

--- hm-modules/desktop/mpv.nix
+++ hm-modules/desktop/mpv.nix
@@ -1,4 +1,4 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 
 let
   cfg = config.modules.mpv;
@@ -9,13 +9,36 @@ in
   config = lib.mkIf cfg.enable {
     programs.mpv = {
       enable = true;
+
       bindings = {
         WHEEL_UP    = "seek  5";
         WHEEL_DOWN  = "seek -5";
         WHEEL_RIGHT = "add volume  2";
         WHEEL_LEFT  = "add volume -2";
+        # Rebind default to override sponsorblock's bind
+        "Shift+g" = "add sub-scale +0.1";
       };
-      config.save-position-on-quit = true;
+
+      config = {
+        osc = false;
+        save-position-on-quit = true;
+      };
+
+      scriptOpts = {
+        uosc = {
+          autohide = true;
+          top_bar_controls = false;
+        };
+        sponsorblock.skip_categories = "interaction,selfpromo,sponsor";
+      };
+
+      scripts = with pkgs.mpvScripts; [
+        mpv-cheatsheet
+        quality-menu
+        sponsorblock
+        thumbfast
+        uosc
+      ];
     };
   };
 }
