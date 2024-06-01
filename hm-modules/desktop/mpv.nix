a4d2967073dcd770ec6934c7cb65b85c90f2bfa7
Mateus Auler <mateusauler@protonmail.com>
2024-05-31 23:32:15 -0300

    mpv: Rebind mouse wheel

--- hm-modules/desktop/mpv.nix
+++ hm-modules/desktop/mpv.nix
@@ -9,6 +9,12 @@ in
   config = lib.mkIf cfg.enable {
     programs.mpv = {
       enable = true;
+      bindings = {
+        WHEEL_UP    = "seek  5";
+        WHEEL_DOWN  = "seek -5";
+        WHEEL_RIGHT = "add volume  2";
+        WHEEL_LEFT  = "add volume -2";
+      };
       config.save-position-on-quit = true;
     };
   };
