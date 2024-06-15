c0d7ec15e182f6d1841f9a268e1ec5645b0836c4
Mateus Auler <mateusauler@protonmail.com>
2024-06-15 01:08:23 -0300

    bat: Fix manpage display

--- hm-modules/common/bat.nix
+++ hm-modules/common/bat.nix
@@ -14,7 +14,13 @@ in
   options.modules.bat.enable = lib.mkEnableOption "bat";
 
   config = lib.mkIf cfg.enable {
-    home.sessionVariables.MANPAGER = "${lib.getExe pkgs.bat} -l man -p";
+    home.sessionVariables = {
+      # Set bat as the default man reader
+      # Also fix the incorrect display of ansi escape sequences (https://github.com/sharkdp/bat/issues/2668#issuecomment-1807916150)
+      MANPAGER = "sh -c 'col -bx | ${lib.getExe pkgs.bat} -l man -p'";
+      MANROFFOPT = "-c";
+    };
+
     programs.bat =
       let
         inherit (config.colorScheme) slug;
