c93b42791938c28555877ad4ae0b84d7273779ad
Mateus Auler <mateusauler@protonmail.com>
2024-06-22 20:37:24 -0300

    copyq: Fix activation script when dots aren't cloned

--- hm-modules/desktop/copyq/default.nix
+++ hm-modules/desktop/copyq/default.nix
@@ -16,11 +16,14 @@ in
       packages = [ pkgs.copyq ];
       # TODO: Link configs with home-manager if not cloning (maybe always link configs?)
       activation.link-copyq-configs =
-        lib.optionalString config.dots.clone lib.hm.dag.entryAfter [ "writeBoundary" ] # bash
+        lib.hm.dag.entryAfter [ "writeBoundary" ] # bash
           ''
             $DRY_RUN_CMD mkdir -p ${config.xdg.configHome}/copyq
-            $DRY_RUN_CMD ln -sf ${config.dots.path}/hm-modules/desktop/copyq/config/* ${config.xdg.configHome}/copyq
+            $DRY_RUN_CMD ${
+              if config.dots.clone then "ln -sf ${config.dots.path}/hm-modules/desktop/copyq" else "cp -r ${./.}"
+            }/config/* ${config.xdg.configHome}/copyq
           '';
+
     };
   };
 }
