863a0660cbc013dd684dbb6e382d396dba424146
Mateus Auler <mateusauler@protonmail.com>
2024-07-14 22:16:57 -0300

    hyprland: Fix scrolling issue by patching in a PR

--- overlays/default.nix
+++ overlays/default.nix
@@ -4,7 +4,7 @@
   # Temporary fix for https://github.com/hyprwm/Hyprland/issues/6681
   (final: prev: {
     hyprland = prev.hyprland.overrideAttrs (old: {
-      patches = (old.patches or [ ]) ++ [ ./hyprland-undo-6317.diff ];
+      patches = (old.patches or [ ]) ++ [ ./hyprland-6881.diff ];
     });
   })
 ]
