d5e645781991cc5131be247cb2544980a2820ca8
Mateus Auler <mateusauler@protonmail.com>
2024-08-15 23:04:55 -0300

    neovim: Remap format to `<leader>F`

--- hm-modules/common/neovim/plugins/lsp.nix
+++ hm-modules/common/neovim/plugins/lsp.nix
@@ -36,7 +36,7 @@ lib.mkIf cfg.enable {
 
           local format = vim.lsp.buf.format
           vim.api.nvim_buf_create_user_command(bufnr, 'Format', format, {})
-          bufmap('<leader>f', format, 'Format')
+          bufmap('<leader>F', format, 'Format')
         '';
       servers = {
         bashls.enable = true;
