0c1032eb403d1c52bce03a722b9acfd2a5689962
Mateus Auler <mateus@auler.dev>
2024-11-18 07:47:29 -0300

    hyprland: Update KeePassXC window rules

--- hm-modules/desktop/hyprland/settings.nix
+++ hm-modules/desktop/hyprland/settings.nix
@@ -118,7 +118,8 @@ in
 
     windowrulev2 = lib.flatten [
       "workspace 2 silent, class:(librewolf)"
-      "workspace 3 silent, class:(KeePassXC)"
+      "workspace 3 silent, class:(KeePassXC), title:(.*)(- KeePassXC), floating:0"
+      "float,              class:(KeePassXC), title:(Access Request)"
       "workspace 4 silent, class:((V|v)esktop)"
       "workspace 5 silent, class:((F|f)erdium)"
       "workspace 6 silent, class:((S|s)team)"
