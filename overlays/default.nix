52a5785c2c0ef26cdc3fdd4dfe6bd3d209d08e4e
Mateus Auler <mateus@auler.dev>
2024-11-18 07:07:25 -0300

    overlays: Use a single function for every overlays

--- overlays/default.nix
+++ overlays/default.nix
@@ -5,9 +5,7 @@
     (final: prev: {
       git-worktree-switcher = final.callPackage ./wt { src = inputs.git-worktree-switcher; };
       satisfactory-mod-manager = final.callPackage ./smm.nix { };
-    })
 
-    (final: prev: {
       ferdium-wayland = prev.ferdium.overrideAttrs (old: {
         postFixup = # bash
           ''
@@ -15,18 +13,14 @@
             sed -i -E "s/Exec=ferdium/Exec=ferdium --enable-features=UseOzonePlatform --ozone-platform=wayland/" $out/share/applications/ferdium.desktop
           '';
       });
-    })
 
-    (final: prev: {
       nsxiv = prev.nsxiv.overrideAttrs (old: {
         postInstall = # bash
           ''
             sed -i -E 's/Exec\s*=\s*nsxiv\s*(.*)$/Exec=nsxiv -a \1/' $out/share/applications/nsxiv.desktop
           '';
       });
-    })
 
-    (final: prev: {
       vimPlugins = prev.vimPlugins // {
         eyeliner-nvim = prev.vimPlugins.eyeliner-nvim.overrideAttrs (old: {
           src = inputs.eyeliner-nvim;
