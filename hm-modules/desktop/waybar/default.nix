de858423ec536fa35fcb3f41bff2f01a75999eae
Mateus Auler <mateus@auler.dev>
2024-12-11 16:14:22 -0300

    WIP extensible waybar modules

--- hm-modules/desktop/waybar/default.nix
+++ hm-modules/desktop/waybar/default.nix
@@ -10,9 +10,64 @@ let
   cfg = config.modules.waybar;
 in
 {
-  options.modules.waybar = {
-    enable = lib.mkEnableOption "Waybar";
-    battery.enable = lib.mkEnableOption "Battery";
+  options.modules.waybar = with lib.options; {
+    enable = mkEnableOption "Waybar";
+    battery.enable = mkEnableOption "Battery";
+    extra-modules =
+      let
+        modules =
+          group:
+          mkOption {
+            description = "Modules to be added to the ${group} group";
+            type =
+              with lib.types;
+              listOf (submodule {
+                options = {
+                  position = mkOption {
+                    description = "Position relative to the reference modules";
+                    type = enum [
+                      "before"
+                      "after"
+                    ];
+                    default = "before";
+                  };
+                  reference-modules = mkOption {
+                    description = "Modules to be used as reference for placement";
+                    type = oneOf [
+                      (listOf str)
+                      str
+                    ];
+                    apply = lib.toList;
+                  };
+                  module-names = mkOption {
+                    description = "Modules to be added";
+                    type = oneOf [
+                      (listOf str)
+                      str
+                    ];
+                    apply = lib.toList;
+                  };
+                };
+              });
+            default = [ ];
+          };
+      in
+      mkOption {
+        type =
+          with lib.types;
+          submodule {
+            options = {
+              left = modules "left";
+              center = modules "center";
+              right = modules "right";
+            };
+          };
+        default = {
+          left = [ ];
+          center = [ ];
+          right = [ ];
+        };
+      };
   };
 
   imports = [
