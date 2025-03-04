c4feef40fa123f1dbc3c6766c403262224c2dab4
Mateus Auler <mateus@auler.dev>
2025-03-04 18:59:15 -0300

    fish: Improve `nr` function
    - Invoke `nix run` with --impure
    - If the first input has a `#`, then it's passed straight to nix run.

--- hm-modules/common/fish/functions/nr.fish
+++ hm-modules/common/fish/functions/nr.fish
@@ -1,3 +1,7 @@
 function nr --description "nix run from nixpkgs"
-	nix run --verbose "nixpkgs#$argv[1]" $argv[2..-1]
+	set runpkg "$argv[1]"
+	if string match -qv '*#*' "$runpkg"
+		set runpkg "nixpkgs#$runpkg"
+	end
+	nix run --impure --verbose "$runpkg" $argv[2..-1]
 end
