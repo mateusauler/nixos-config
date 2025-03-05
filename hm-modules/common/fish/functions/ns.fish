55e364ca0980a7b060c883d67a6d97a6fbd2c5d8
Mateus Auler <mateus@auler.dev>
2025-03-05 09:42:15 -0300

    fish: Improve `ns` function
    - Invoke `nix shell` with --impure
    - If an input has a `#`, then it's passed straight to nix shell.

--- hm-modules/common/fish/functions/ns.fish
+++ hm-modules/common/fish/functions/ns.fish
@@ -1,11 +1,15 @@
 function ns --description "Run nix shell with the packages provided in the argument set"
 	set pkgs
 	for pkg in $argv[1..-1]
-		set pkgs $pkgs "nixpkgs#$pkg"
+		if string match -qv '*#*' "$pkg"
+			set pkgs $pkgs "nixpkgs#$pkg"
+		else
+			set pkgs $pkgs "$pkg"
+		end
 	end
 	set names $argv[1]
 	for pkg in $argv[2..-1]
 		set names "$names, $pkg"
 	end
-	IN_NIX_SHELL=1 NIX_SHELL_PKGS="$(test -n "$NIX_SHELL_PKGS" && echo "$NIX_SHELL_PKGS :: ")$names" nix shell --verbose $pkgs --command $SHELL
+	IN_NIX_SHELL=1 NIX_SHELL_PKGS="$(test -n "$NIX_SHELL_PKGS" && echo "$NIX_SHELL_PKGS :: ")$names" nix shell --impure --verbose $pkgs --command $SHELL
 end
