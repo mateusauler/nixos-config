2dc1f3560190a022075ee515959383400aef4f2d
Mateus Auler <mateus@auler.dev>
2025-04-02 00:19:25 -0300

    neovim: Use `gx` nixvim module

--- hm-modules/common/neovim/plugins/oil.nix
+++ hm-modules/common/neovim/plugins/oil.nix
@@ -1,20 +1,16 @@
 {
   config,
-  inputs,
   lib,
-  pkgs,
   ...
 }:
 
 let
   cfg = config.modules.neovim;
-  enabled = config.programs.nixvim.plugins.oil.enable;
+  inherit (config.programs.nixvim.plugins.oil) enable;
 in
 lib.mkIf cfg.enable {
   programs.nixvim = {
-    globals = lib.optionalAttrs enabled { netrw_nogx = 1; };
-
-    keymaps = lib.optionals enabled [
+    keymaps = lib.optionals enable [
       {
         mode = "n";
         key = "-";
@@ -31,34 +27,25 @@ lib.mkIf cfg.enable {
       }
     ];
 
-    extraConfigLua =
-      lib.optionalString enabled # lua
-        ''
-          require("gx").setup({
-            handler_options = { search_engine = "duckduckgo" },
-          })
-        '';
-
-    plugins.oil.settings = {
-      default_file_explorer = true;
-      experimental_watch_for_changes = true;
-      skip_confirm_for_simple_edits = true;
-      view_options = {
-        show_hidden = true;
-        is_always_hidden = # lua
-          ''
-            function(name, _)
-              return name == '..' or name == '.git'
-            end
-          '';
+    plugins = {
+      gx = {
+        inherit enable;
+        settings.handler_options.search_engine = "duckduckgo";
+      };
+      oil.settings = {
+        default_file_explorer = true;
+        experimental_watch_for_changes = true;
+        skip_confirm_for_simple_edits = true;
+        view_options = {
+          show_hidden = true;
+          is_always_hidden = # lua
+            ''
+              function(name, _)
+                return name == '..' or name == '.git'
+              end
+            '';
+        };
       };
     };
-
-    extraPlugins = lib.optional enabled (
-      pkgs.vimUtils.buildVimPlugin {
-        name = "gx";
-        src = inputs.gx;
-      }
-    );
   };
 }
