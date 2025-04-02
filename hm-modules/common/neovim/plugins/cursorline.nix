a61e1854e67aa8712fecc3fe6f581063afa58777
Mateus Auler <mateus@auler.dev>
2025-04-02 20:01:04 -0300

    neovim: Move cursorline options to cursorline.settings

--- hm-modules/common/neovim/plugins/cursorline.nix
+++ hm-modules/common/neovim/plugins/cursorline.nix
@@ -4,7 +4,7 @@ let
   cfg = config.modules.neovim;
 in
 lib.mkIf cfg.enable {
-  programs.nixvim.plugins.cursorline = {
+  programs.nixvim.plugins.cursorline.settings = {
     cursorline = {
       timeout = 450;
       number = true;
