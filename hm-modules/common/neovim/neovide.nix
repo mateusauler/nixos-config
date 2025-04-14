f8c0ca60761bfcafddfbcfd0546e9beec3261949
Mateus Auler <mateus@auler.dev>
2025-04-14 11:38:57 -0300

    neovide: neovide_transparency => neovide_opacity

--- hm-modules/common/neovim/neovide.nix
+++ hm-modules/common/neovim/neovide.nix
@@ -21,7 +21,7 @@ lib.mkIf (cfg.enable && cfg.neovide.enable) {
     opts.guifont = lib.mkDefault (
       with config.stylix.fonts; "${monospace.name}:h${toString sizes.terminal}"
     );
-    globals.neovide_transparency = lib.mkDefault config.stylix.opacity.terminal;
+    globals.neovide_opacity = lib.mkDefault config.stylix.opacity.terminal;
   };
 
   xdg.configFile."neovide/config.toml".source = conf;
