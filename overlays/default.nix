cd9a143549efcd7f64e0e1e5945296d2af9d9a7f
Mateus Auler <mateus@auler.dev>
2025-04-26 13:08:55 -0300

    overlays: Go back to using default CopyQ

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,4 +1,4 @@
-{ inputs, pkgs-stable, ... }:
+{ inputs, ... }:
 
 {
   nixpkgs.overlays = [
@@ -9,8 +9,6 @@
             sed -i -E 's/Exec\s*=\s*nsxiv\s*(.*)$/Exec=nsxiv -a \1/' $out/share/applications/nsxiv.desktop
           '';
       });
-
-      copyq = pkgs-stable.copyq; # https://github.com/NixOS/nixpkgs/pull/398926
     })
 
     inputs.jujutsu.overlays.default
