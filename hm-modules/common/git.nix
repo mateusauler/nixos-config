c5a4e337901a692c126fb54c1beb87ac3a914a9c
Mateus Auler <mateus@auler.dev>
2025-05-04 22:52:29 -0300

    hm: Move module-dependent fish abbrs to their respecive modules

--- hm-modules/common/git.nix
+++ hm-modules/common/git.nix
@@ -18,9 +18,13 @@ in
   };
 
   config = {
+    shell-aliases = {
+      gua = "git remote | sed '/^blacklist/{d}' | xargs -L1 git push --all";
+      gsu = "git remote | sed '/^blacklist/{d}' | xargs -I {} git branch -u {}/$(git branch --show-current) $(git branch --show-current)";
+    };
+
     programs =
       {
-        lazygit.enable = cfg.lazygit;
         git = {
           enable = true;
           lfs.enable = true;
@@ -62,6 +66,59 @@ in
             init.defaultBranch = "master";
             mergetool.keepBackup = false;
           };
+        };
+
+        lazygit.enable = cfg.lazygit;
+
+        fish.shellAbbrs = {
+          g = "git";
+
+          gaa = "git add -A";
+          ga = "git add";
+          gap = "git add -p";
+          gb = "git branch";
+          gcaa = "git commit -a --amend";
+          gcaan = "git commit -a --amend --no-edit";
+          gca = "git commit -a";
+          gcam = "git commit --amend";
+          gcan = "git commit --amend --no-edit";
+          gc = "git commit";
+          gdc = "git diff --cached";
+          gd = "git diff";
+          gf = "git fetch";
+          gg = "git grep";
+          ggi = "git grep -i";
+          ghb = "git checkout -b";
+          gh = "git checkout";
+          gl = "git log";
+          gll = "git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative";
+          glp = "git log -p";
+          gm = "git merge";
+          gmt = "git mergetool";
+          gp = "git pull";
+          gr = "git rebase";
+          gra = "git rebase --abort";
+          grc = "git rebase --continue";
+          gri = "git rebase -i";
+          gre = "git reset";
+          grh = "git reset --hard";
+          gs = "git status";
+          gsh = "git show";
+          gsd = "git stash drop";
+          gsl = "git stash list";
+          gsp = "git stash pop";
+          gst = "git stash";
+          gsu = "git stash -u";
+          gu = "git push";
+          gw = "git worktree";
+          gwa = "git worktree add";
+          gwl = "git worktree list";
+          gwp = "git worktree prune";
+          gwr = "git worktree remove";
+
+          cg = "cd $(git rev-parse --show-toplevel 2> /dev/null || pwd | sed 's|/\\.git.*$||')";
+
+          lg = lib.mkIf cfg.lazygit "lazygit";
         };
       }
       // lib.optionalAttrs (config.programs ? git-worktree-switcher) {
--- hm-modules/common/shell-aliases.nix
+++ hm-modules/common/shell-aliases.nix
@@ -4,19 +4,7 @@
   options.shell-aliases = lib.mkOption { type = with lib.types; attrsOf str; };
 
   config.shell-aliases = {
-    gua = # bash
-      ''
-        git remote | sed '/^blacklist/{d}' | xargs -L1 git push --all
-      '';
-    gsu = # bash
-      ''
-        git remote | sed '/^blacklist/{d}' | xargs -I {} git branch -u {}/(git branch --show-current) (git branch --show-current)
-      '';
     cleanup-after-bad-patch = "${pkgs.fd}/bin/fd '\.(orig|rej)$' -X rm";
     wget = "wget --hsts-file $XDG_CACHE_HOME/wget-hsts";
-    sqlite3 = "sqlite3 -init $XDG_CONFIG_HOME/sqlite3/sqliterc";
-
-    rm = "rm -I";
-    cp = "cp -r";
   };
 }
