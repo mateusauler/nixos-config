16ceec8a76af3b3d5acdce49e3ff5d1f19e30615
Mateus Auler <mateus@auler.dev>
2025-05-10 09:40:08 -0300

    overlays: Fix anytype

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,13 +1,13 @@
 {
   inputs,
+  lib,
   nixpkgs-channel,
-  pkgs,
   pkgs-unstable,
   ...
 }:
 
 {
-  nixpkgs.overlays = [
+  nixpkgs.overlays = lib.flatten [
     (final: prev: {
       nsxiv = prev.nsxiv.overrideAttrs (old: {
         postInstall = # bash
@@ -15,8 +15,9 @@
             sed -i -E 's/Exec\s*=\s*nsxiv\s*(.*)$/Exec=nsxiv -a \1/' $out/share/applications/nsxiv.desktop
           '';
       });
-      waybar-git = inputs.waybar.packages.${pkgs.stdenv.hostPlatform.system}.waybar;
+      waybar-git = inputs.waybar.packages.${prev.stdenv.hostPlatform.system}.waybar;
     })
+
     (
       if (nixpkgs-channel == "unstable") then
         inputs.jujutsu.overlays.default
@@ -24,5 +25,18 @@
         # Jujutsu package in stable is insecure
         (_: _: { inherit (pkgs-unstable) jujutsu lazyjj; })
     )
+
+    # https://github.com/NixOS/nixpkgs/issues/405673
+    (lib.optional (nixpkgs-channel == "unstable") (
+      final: prev: {
+        inherit
+          (import inputs.updated-anytype-nixpkgs {
+            inherit (prev.stdenv.hostPlatform) system;
+            config.allowUnfree = true;
+          })
+          anytype
+          ;
+      }
+    ))
   ];
 }
