3a5b664b9c9d55ee908f8107c2b859eaf60f7559
Mateus Auler <mateus@auler.dev>
2025-05-21 09:27:34 -0300

    virt-manager: Better configs

--- modules/desktop/virt-manager/default.nix
+++ modules/desktop/virt-manager/default.nix
@@ -21,8 +21,20 @@ in
       libvirtd = {
         enable = true;
         onBoot = mkDefault "ignore";
-        qemu.runAsRoot = mkDefault false;
-        qemu.ovmf.enable = true;
+        qemu = {
+          package = pkgs.qemu_kvm;
+          runAsRoot = mkDefault false;
+          swtpm.enable = true;
+          ovmf = {
+            enable = true;
+            packages = [
+              (pkgs.OVMF.override {
+                secureBoot = true;
+                tpmSupport = true;
+              }).fd
+            ];
+          };
+        };
       };
     };
 
