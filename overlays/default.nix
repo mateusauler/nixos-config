cfd264e949380ec28afca2cbbc17300a4e29eaa3
Mateus Auler <mateus@auler.dev>
2025-05-31 21:17:18 -0300

    overlays: Fix libplacebo issue

--- overlays/default.nix
+++ overlays/default.nix
@@ -17,6 +17,24 @@
       waybar-git = inputs.waybar.packages.${prev.stdenv.hostPlatform.system}.waybar;
     })
 
-    (lib.optional (nixpkgs-channel == "unstable") inputs.jujutsu.overlays.default)
+    (lib.optionals (nixpkgs-channel == "unstable") [
+      inputs.jujutsu.overlays.default
+
+      # https://github.com/NixOS/nixpkgs/issues/412382#issuecomment-2924903019
+      (final: prev: {
+        mpv-unwrapped = prev.mpv-unwrapped.override {
+          libplacebo = prev.libplacebo.overrideAttrs (old: rec {
+            version = "7.349.0";
+            src = prev.fetchFromGitLab {
+              domain = "code.videolan.org";
+              owner = "videolan";
+              repo = "libplacebo";
+              rev = "v${version}";
+              hash = "sha256-mIjQvc7SRjE1Orb2BkHK+K1TcRQvzj2oUOCUT4DzIuA=";
+            };
+          });
+        };
+      })
+    ])
   ];
 }
