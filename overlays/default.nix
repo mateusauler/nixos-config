e49016b74575747ec5673f4b0074354833ed65bf
Mateus Auler <mateus@auler.dev>
2025-06-07 11:55:23 -0300

    overlays: Remove mpv libplacebo fix

--- overlays/default.nix
+++ overlays/default.nix
@@ -19,22 +19,6 @@
 
     (lib.optionals (nixpkgs-channel == "unstable") [
       inputs.jujutsu.overlays.default
-
-      # https://github.com/NixOS/nixpkgs/issues/412382#issuecomment-2924903019
-      (final: prev: {
-        mpv-unwrapped = prev.mpv-unwrapped.override {
-          libplacebo = prev.libplacebo.overrideAttrs (old: rec {
-            version = "7.349.0";
-            src = prev.fetchFromGitLab {
-              domain = "code.videolan.org";
-              owner = "videolan";
-              repo = "libplacebo";
-              rev = "v${version}";
-              hash = "sha256-mIjQvc7SRjE1Orb2BkHK+K1TcRQvzj2oUOCUT4DzIuA=";
-            };
-          });
-        };
-      })
     ])
   ];
 }
