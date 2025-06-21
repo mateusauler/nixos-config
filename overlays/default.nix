25d1a60604c6de01c8882162985e313ac771af58
Mateus Auler <mateus@auler.dev>
2025-06-21 13:15:59 -0300

    Fix rocm clr

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,6 +1,7 @@
 {
   inputs,
   lib,
+  nixpkgs-channel,
   ...
 }:
 
@@ -15,5 +16,13 @@
       });
       waybar-git = inputs.waybar.packages.${prev.stdenv.hostPlatform.system}.waybar;
     })
+
+    (lib.optional (nixpkgs-channel == "unstable") (
+      final: prev: {
+        inherit (import inputs.nixpkgs-pr-418461 { inherit (prev.stdenv.hostPlatform) system; })
+          rocmPackages
+          ;
+      }
+    ))
   ];
 }
