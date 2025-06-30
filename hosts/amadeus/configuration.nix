4fa8178e1425c9f2dd2a156ffb12e71da7b161dd
Mateus Auler <mateus@auler.dev>
2025-06-29 21:43:24 -0300

    amadeus: Only allow syncthing gui access via netbird

--- hosts/amadeus/configuration.nix
+++ hosts/amadeus/configuration.nix
@@ -11,19 +11,11 @@ in
   imports = [
     ./hardware-configuration.nix
     ./rustdesk.nix
+    ./syncthing.nix
     ./zfs.nix
   ];
 
   modules = lib.enableModules module-names;
-
-  services.syncthing = {
-    configDir = "/var/lib/syncthing/config";
-    dataDir = "/tank/syncthing";
-    guiAddress = "0.0.0.0:8384";
-  };
-
-  networking.firewall.allowedTCPPorts = [ 8384 ];
-
   enabledUsers = [ "mateus" ];
 
   networking.hostName = "amadeus";
