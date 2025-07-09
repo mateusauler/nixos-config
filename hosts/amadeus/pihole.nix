991b262b87a07bde948adc2700f279ac49054d02
Mateus Auler <mateus@auler.dev>
2025-07-09 16:38:34 -0300

    pihole: Add lists

--- hosts/amadeus/pihole.nix
+++ hosts/amadeus/pihole.nix
@@ -44,6 +44,60 @@
               theme = "default-darker";
             };
           };
+
+          lists = [
+            {
+              url = "https://raw.githubusercontent.com/anudeepND/whitelist/refs/heads/master/domains/whitelist.txt";
+              type = "allow";
+            }
+
+            { url = "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"; }
+
+            # https://firebog.net/
+
+            # Suspicious
+            { url = "https://raw.githubusercontent.com/PolishFiltersTeam/KADhosts/master/KADhosts.txt"; }
+            { url = "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Spam/hosts"; }
+            { url = "https://v.firebog.net/hosts/static/w3kbl.txt"; }
+
+            # Ads
+            { url = "https://adaway.org/hosts.txt"; }
+            { url = "https://v.firebog.net/hosts/AdguardDNS.txt"; }
+            { url = "https://v.firebog.net/hosts/Admiral.txt"; }
+            { url = "https://raw.githubusercontent.com/anudeepND/blacklist/master/adservers.txt"; }
+            { url = "https://v.firebog.net/hosts/Easylist.txt"; }
+            {
+              url = "https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext";
+            }
+            { url = "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/UncheckyAds/hosts"; }
+            { url = "https://raw.githubusercontent.com/bigdargon/hostsVN/master/hosts"; }
+
+            # Tracking
+            { url = "https://v.firebog.net/hosts/Easyprivacy.txt"; }
+            { url = "https://v.firebog.net/hosts/Prigent-Ads.txt"; }
+            { url = "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.2o7Net/hosts"; }
+            { url = "https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/hosts/spy.txt"; }
+            { url = "https://hostfiles.frogeye.fr/firstparty-trackers-hosts.txt"; }
+
+            # Malicious
+            {
+              url = "https://raw.githubusercontent.com/DandelionSprout/adfilt/master/Alternate%20versions%20Anti-Malware%20List/AntiMalwareHosts.txt";
+            }
+            { url = "https://v.firebog.net/hosts/Prigent-Crypto.txt"; }
+            { url = "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Risk/hosts"; }
+            {
+              url = "https://bitbucket.org/ethanr/dns-blacklists/raw/8575c9f96e5b4a1308f2f12394abd86d0927a4a0/bad_lists/Mandiant_APT1_Report_Appendix_D.txt";
+            }
+            { url = "https://phishing.army/download/phishing_army_blocklist_extended.txt"; }
+            { url = "https://gitlab.com/quidsup/notrack-blocklists/raw/master/notrack-malware.txt"; }
+            { url = "https://v.firebog.net/hosts/RPiList-Malware.txt"; }
+            { url = "https://raw.githubusercontent.com/Spam404/lists/master/main-blacklist.txt"; }
+            {
+              url = "https://raw.githubusercontent.com/AssoEchap/stalkerware-indicators/master/generated/hosts";
+            }
+            { url = "https://urlhaus.abuse.ch/downloads/hostfile/"; }
+            { url = "https://lists.cyberhost.uk/malware.txt"; }
+          ];
         };
 
         services.pihole-web = {
