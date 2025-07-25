eb18ffc13e059998868ab11d326a16b2cebb7efc
Mateus Auler <mateus@auler.dev>
2025-07-24 21:59:10 -0300

    xdg: More compliance

--- overlays/default.nix
+++ overlays/default.nix
@@ -24,6 +24,32 @@
           install -Dm755 "${./niri-session}" "$out/bin/niri-session"
         '';
       };
+      android-tools =
+        let
+          script = pkgs.writeShellScript "adb" ''
+            HOME=''${XDG_DATA_HOME:-$HOME/.local/share}/android exec ${prev.android-tools}/bin/adb "$@"
+          '';
+        in
+        pkgs.symlinkJoin {
+          inherit (prev.android-tools) name;
+          paths = [ prev.android-tools ];
+          postBuild = ''
+            install -Dm755 "${script}" "$out/bin/adb"
+          '';
+        };
+      wget =
+        let
+          script = pkgs.writeShellScript "wget" ''
+            exec ${lib.getExe prev.wget} --hsts-file ''${XDG_CACHE_HOME:-$HOME/.cache}/wget-hsts "$@"
+          '';
+        in
+        pkgs.symlinkJoin {
+          inherit (prev.wget) name;
+          paths = [ prev.wget ];
+          postBuild = ''
+            install -Dm755 "${script}" "$out/bin/wget"
+          '';
+        };
     })
   ];
 }
