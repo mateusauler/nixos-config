f5b27793c6f10290595122f9933ab2e2ed55fb7c
Mateus Auler <mateus@auler.dev>
2025-08-23 23:57:59 -0300

    zswap: Use new sysfs module

--- modules/common/zswap.nix
+++ modules/common/zswap.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  options,
   ...
 }:
 
@@ -10,13 +11,21 @@ in
 {
   options.modules.zswap.enable = lib.mkEnableOption "ZSwap";
 
-  config = lib.mkIf cfg.enable {
-    # https://github.com/NixOS/nixpkgs/issues/119244#issuecomment-1250321791
-    systemd.services.zswap = {
-      description = "Enable ZSwap";
-      enable = true;
-      wantedBy = [ "basic.target" ];
-      script = "echo 1 > /sys/module/zswap/parameters/enabled";
-    };
-  };
+  config = lib.mkIf cfg.enable (
+    # FIXME: Implemented only on 25.11
+    if options.boot.kernel ? sysfs then
+      {
+        boot.kernel.sysfs.module.zswap.parameters.enabled = true;
+      }
+    else
+      {
+        # https://github.com/NixOS/nixpkgs/issues/119244#issuecomment-1250321791
+        systemd.services.zswap = {
+          description = "Enable ZSwap";
+          enable = true;
+          wantedBy = [ "basic.target" ];
+          script = "echo 1 > /sys/module/zswap/parameters/enabled";
+        };
+      }
+  );
 }
