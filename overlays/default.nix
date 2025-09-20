8e682091fe6b75e96e5fb08e8cd7085e9085d742
Mateus Auler <mateus@auler.dev>
2025-09-20 13:00:44 -0300

    overlays: Overlay nsxiv with symlinkJoin

--- overlays/default.nix
+++ overlays/default.nix
@@ -9,12 +9,13 @@
   nixpkgs.overlays = lib.flatten [
     private-config.overlays
     (final: prev: {
-      nsxiv = prev.nsxiv.overrideAttrs (old: {
-        postInstall = # bash
-          ''
-            sed -i -E 's/Exec\s*=\s*nsxiv\s*(.*)$/Exec=nsxiv -a \1/' $out/share/applications/nsxiv.desktop
-          '';
-      });
+      nsxiv = pkgs.symlinkJoin {
+        inherit (prev.nsxiv) name;
+        paths = [ prev.nsxiv ];
+        postInstall = ''
+          sed -i -E 's/Exec\s*=\s*nsxiv\s*(.*)$/Exec=nsxiv -a \1/' $out/share/applications/nsxiv.desktop
+        '';
+      };
       niri = pkgs.symlinkJoin {
         inherit (prev.niri) name cargoBuildNoDefaultFeatures cargoBuildFeatures;
         paths = [ prev.niri ];
