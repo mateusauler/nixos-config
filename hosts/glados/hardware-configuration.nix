e8e307f2b087638efe628e7a81cae3d0bfe281c1
Mateus Auler <mateus@auler.dev>
2025-10-11 10:53:38 -0300

    glados: Remove rocm configuration

--- hosts/glados/hardware-configuration.nix
+++ hosts/glados/hardware-configuration.nix
@@ -1,7 +1,5 @@
 {
   config,
-  lib,
-  pkgs,
   modulesPath,
   ...
 }:
@@ -54,30 +52,12 @@
 
   swapDevices = [ ];
 
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  powerManagement.cpuFreqGovernor = lib.mkDefault "ondemand";
-  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  nixpkgs.hostPlatform = "x86_64-linux";
+  powerManagement.cpuFreqGovernor = "ondemand";
+  hardware.cpu.amd.updateMicrocode = config.hardware.enableRedistributableFirmware;
 
-  # https://wiki.nixos.org/w/index.php?title=AMD_GPU
-  systemd.tmpfiles.rules =
-    let
-      rocmEnv = pkgs.symlinkJoin {
-        name = "rocm-combined";
-        paths = with pkgs.rocmPackages; [
-          rocblas
-          hipblas
-          clr
-        ];
-      };
-    in
-    [ "L+    /opt/rocm   -    -    -     -    ${rocmEnv}" ];
-
-  hardware.graphics = with pkgs; {
+  hardware.graphics = {
     enable = true;
     enable32Bit = true;
-    extraPackages = [
-      rocmPackages.clr
-      rocmPackages.clr.icd
-    ];
   };
 }
